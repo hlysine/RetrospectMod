101ad9ce85ff4a2ec6b79357cc3ee33536e075ed
Henry Lin <henry.ys.lin@gmail.com>
2022-03-31 12:05:34 +0800

    Add TapTheFuture

---
+++ src/main/java/theRetrospect/cards/TapTheFuture.java
@@ -0,0 +1,61 @@
+package theRetrospect.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRetrospect.RetrospectMod;
+import theRetrospect.actions.TransmuteAction;
+
+import static theRetrospect.RetrospectMod.makeCardPath;
+
+public class TapTheFuture extends AbstractRetrospectCard {
+
+    public static final String ID = RetrospectMod.makeID(TapTheFuture.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    public static final String IMG = makeCardPath("Skill.png");
+
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+
+    private static final int COST = 0;
+    private static final int ENERGY_GAIN = 2;
+    private static final int UPGRADE_ENERGY_GAIN = 1;
+    private static final int EMPTY_CARD_COUNT = 2;
+
+    public TapTheFuture() {
+        super(ID, IMG, COST, TYPE, RARITY, TARGET);
+
+        this.baseMagicNumber = ENERGY_GAIN;
+        this.magicNumber = baseMagicNumber;
+        this.cardsToPreview = new Empty();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainEnergyAction(this.magicNumber));
+        addToBot(new MakeTempCardInDrawPileAction(this.cardsToPreview.makeStatEquivalentCopy(), EMPTY_CARD_COUNT, true, true));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(UPGRADE_ENERGY_GAIN);
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+}
