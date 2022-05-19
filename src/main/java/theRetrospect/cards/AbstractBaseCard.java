9c4fa485f7724c137d822eb8f6a552f56c3ef3d8
Henry Lin <henry.ys.lin@gmail.com>
2022-05-19 22:35:42 +0800

    Move delusional to AbstractBaseCard

--- src/main/java/theRetrospect/cards/AbstractBaseCard.java
+++ src/main/java/theRetrospect/cards/AbstractBaseCard.java
@@ -1,10 +1,14 @@
 package theRetrospect.cards;
 
 import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 
 public abstract class AbstractBaseCard extends CustomCard {
+
+    public boolean delusional = false;
 
     public AbstractBaseCard(final String id,
                             final String img,
@@ -15,5 +19,12 @@ public abstract class AbstractBaseCard extends CustomCard {
                             final CardTarget target) {
 
         super(id, languagePack.getCardStrings(id).NAME, img, cost, languagePack.getCardStrings(id).DESCRIPTION, type, color, rarity, target);
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        super.triggerWhenDrawn();
+        if (delusional)
+            addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
     }
 }
--- src/main/java/theRetrospect/cards/AbstractRetrospectCard.java
+++ src/main/java/theRetrospect/cards/AbstractRetrospectCard.java
@@ -1,14 +1,10 @@
 package theRetrospect.cards;
 
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import theRetrospect.characters.TheRetrospect;
 
 public abstract class AbstractRetrospectCard extends AbstractBaseCard {
 
     public static final CardColor COLOR = TheRetrospect.Enums.RETROSPECT_CARD_VIOLET;
-
-    public boolean delusional = false;
 
     public AbstractRetrospectCard(final String id,
                                   final String img,
@@ -18,12 +14,5 @@ public abstract class AbstractRetrospectCard extends AbstractBaseCard {
                                   final CardTarget target) {
 
         super(id, img, cost, type, COLOR, rarity, target);
-    }
-
-    @Override
-    public void triggerWhenDrawn() {
-        super.triggerWhenDrawn();
-        if (delusional)
-            addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
     }
 }
