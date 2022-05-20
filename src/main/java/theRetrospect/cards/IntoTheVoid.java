ec7a8d8a4a83512bf7439603452be7fd3c0b656a
Henry Lin <henry.ys.lin@gmail.com>
2022-05-20 23:17:06 +0800

    Rework IntoTheVoid to make use of the new timeline targeting

--- src/main/java/theRetrospect/cards/IntoTheVoid.java
+++ src/main/java/theRetrospect/cards/IntoTheVoid.java
@@ -4,12 +4,12 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import theRetrospect.RetrospectMod;
 import theRetrospect.actions.CollapseTimelineAction;
 import theRetrospect.actions.TriggerTimelineAction;
 import theRetrospect.minions.TimelineMinion;
 import theRetrospect.util.MinionUtils;
+import theRetrospect.util.TimelineTargeting;
 
 import static theRetrospect.RetrospectMod.makeCardPath;
 
@@ -25,35 +25,40 @@ public class IntoTheVoid extends AbstractRetrospectCard {
 
 
     private static final CardRarity RARITY = CardRarity.BASIC;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = TimelineTargeting.TIMELINE;
     private static final CardType TYPE = CardType.SKILL;
+
+    private static final int BASE_TRIGGER_COUNT = 1;
+    private static final int UPGRADE_TRIGGER_COUNT = 1;
 
     private static final int COST = 1;
 
     public IntoTheVoid() {
         super(ID, IMG, COST, TYPE, RARITY, TARGET);
 
-        this.selfRetain = true;
-        this.exhaust = true;
+        magicNumber = baseMagicNumber = BASE_TRIGGER_COUNT;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        MonsterGroup minions = MinionUtils.getMinions(p);
-        for (AbstractMonster monster : minions.monsters) {
-            if (monster instanceof TimelineMinion) {
-                TimelineMinion minion = (TimelineMinion) monster;
-                addToBot(new TriggerTimelineAction(minion, false));
-                addToBot(new CollapseTimelineAction(minion));
-            }
-        }
+        TimelineMinion target = (TimelineMinion) TimelineTargeting.getTarget(this);
+
+        addToBot(new TriggerTimelineAction(target, magicNumber, false));
+        addToBot(new CollapseTimelineAction(target));
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        boolean canUse = super.canUse(p, m);
+        if (!canUse) return false;
+        return MinionUtils.getMinions(p).monsters.size() != 0;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.isInnate = true;
+            this.upgradeMagicNumber(UPGRADE_TRIGGER_COUNT);
             this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
