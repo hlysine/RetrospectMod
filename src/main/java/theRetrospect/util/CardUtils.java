19d98fc33dce913dc6253e58b5bb5cdda77d2059
Henry Lin <henry.ys.lin@gmail.com>
2022-05-20 22:55:50 +0800

    Use ReflectionHacks helper methods

--- src/main/java/theRetrospect/util/CardUtils.java
+++ src/main/java/theRetrospect/util/CardUtils.java
@@ -1,35 +1,18 @@
 package theRetrospect.util;
 
+import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import theRetrospect.patches.CardAddFieldsPatch;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 public class CardUtils {
-    private static final Field hoveredField;
-
-    static {
-        Field field;
-        try {
-            field = AbstractCard.class.getDeclaredField("hovered");
-            field.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            field = null;
-            e.printStackTrace();
-        }
-        hoveredField = field;
-    }
 
     public static boolean getHovered(AbstractCard card) {
-        try {
-            return (boolean) hoveredField.get(card);
-        } catch (IllegalAccessException e) {
-            return false;
-        }
+        return ReflectionHacks.getPrivate(card, AbstractCard.class, "hovered");
     }
 
     public static CardPlaySource getPlaySource(AbstractCard card) {
