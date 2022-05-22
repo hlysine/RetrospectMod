b0be78ab9c993f37452be4c7e029f56fdd6ef4c9
Henry Lin <henry.ys.lin@gmail.com>
2022-05-22 14:22:50 +0800

    Implement Avert

---
+++ src/main/java/theRetrospect/cards/Avert.java
@@ -0,0 +1,53 @@
+package theRetrospect.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRetrospect.RetrospectMod;
+import theRetrospect.actions.ConstructTimelineAction;
+
+import static theRetrospect.RetrospectMod.makeCardPath;
+
+public class Avert extends AbstractTimelineCard {
+
+    public static final String ID = RetrospectMod.makeID(Avert.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    public static final String IMG = makeCardPath("avert.png");
+
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+
+    private static final int COST = 2;
+    private static final int BASE_BLOCK = 10;
+    private static final int UPGRADE_BLOCK = 5;
+
+    public Avert() {
+        super(ID, IMG, COST, TYPE, RARITY, TARGET);
+
+        block = baseBlock = BASE_BLOCK;
+        timelineCount = baseTimelineCount = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new ConstructTimelineAction(this));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(UPGRADE_BLOCK);
+            this.initializeDescription();
+        }
+    }
+}
