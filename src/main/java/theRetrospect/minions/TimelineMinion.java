cfe716c9c490c452a2a7613e36ee3e26bf3ed52e
Henry Lin <henry.ys.lin@gmail.com>
2022-05-22 11:36:08 +0800

    Fix a crash caused by Friendly Minions version downgrade
    Version downgrade is caused by the steam workshop version of FM not up to date with git

--- src/main/java/theRetrospect/minions/TimelineMinion.java
+++ src/main/java/theRetrospect/minions/TimelineMinion.java
@@ -21,13 +21,16 @@ public class TimelineMinion extends AbstractMinionWithCards {
 
     public static final String ID = RetrospectMod.makeID(TimelineMinion.class.getSimpleName());
     private static final CharacterStrings minionStrings = CardCrawlGame.languagePack.getCharacterString(ID);
+    /**
+     * Placeholder image to avoid a crash in Friendly Minions
+     */
     public static final String IMG = "theRetrospectResources/images/char/retrospectCharacter/Spriter/object1.png";
     public static final String NAME = minionStrings.NAMES[0];
 
     private float auraEffectTimer = 0.5f;
 
     public TimelineMinion(AbstractPlayer summoner, List<AbstractCard> cards, int offsetX, int offsetY, int maxHealth) {
-        super(NAME, ID, maxHealth, 0, 0, 120, 120, null, offsetX, offsetY);
+        super(NAME, ID, maxHealth, 0, 0, 120, 120, IMG, offsetX, offsetY);
 
         this.scale = 0.5f;
 
