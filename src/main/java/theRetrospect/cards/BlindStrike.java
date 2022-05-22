c590339c8404674cde423ef7781214f7bce235d5
Henry Lin <henry.ys.lin@gmail.com>
2022-05-22 17:21:00 +0800

    Implement Blind Strike

---
+++ src/main/java/theRetrospect/cards/BlindStrike.java
@@ -0,0 +1,52 @@
+package theRetrospect.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRetrospect.RetrospectMod;
+
+import static theRetrospect.RetrospectMod.makeCardPath;
+
+public class BlindStrike extends AbstractRetrospectCard {
+
+    public static final String ID = RetrospectMod.makeID(BlindStrike.class.getSimpleName());
+
+    public static final String IMG = makeCardPath("blind_strike.png");
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+
+    private static final int COST = 1;
+    private static final int BASE_DAMAGE = 12;
+    private static final int UPGRADE_DAMAGE = 4;
+
+    public BlindStrike() {
+        super(ID, IMG, COST, TYPE, RARITY, TARGET);
+
+        damage = baseDamage = BASE_DAMAGE;
+
+        cardsToPreview = new Dazed();
+
+        this.tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        addToTop(new MakeTempCardInDrawPileAction(new Dazed(), 1, true, true));
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            initializeDescription();
+        }
+    }
+}
