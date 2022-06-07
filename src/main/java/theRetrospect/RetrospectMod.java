5698d58826a3ca66ce7ad7b0ab9b43ddce454d24
Henry Lin <henry.ys.lin@gmail.com>
2022-06-07 21:15:45 +0800

    Remove custom glow color
    canUse removes glow when the card is unplayable, which is already enough

--- src/main/java/theRetrospect/RetrospectMod.java
+++ src/main/java/theRetrospect/RetrospectMod.java
@@ -4,7 +4,6 @@ import basemod.AutoAdd;
 import basemod.BaseMod;
 import basemod.abstracts.CustomRelic;
 import basemod.eventUtil.AddEventParams;
-import basemod.helpers.CardBorderGlowManager;
 import basemod.interfaces.*;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
@@ -26,7 +25,6 @@ import theRetrospect.potions.TimelinePotion;
 import theRetrospect.relics.AbstractBaseRelic;
 import theRetrospect.util.IDCheckDontTouchPls;
 import theRetrospect.util.TextureLoader;
-import theRetrospect.util.TimelineCardGlow;
 import theRetrospect.util.TimelineTargeting;
 import theRetrospect.variables.TimelineCountVariable;
 
@@ -208,8 +206,6 @@ public class RetrospectMod implements
                 .create();
 
         BaseMod.addEvent(eventParams);
-
-        CardBorderGlowManager.addGlowInfo(new TimelineCardGlow());
     }
 
     public void receiveEditPotions() {
