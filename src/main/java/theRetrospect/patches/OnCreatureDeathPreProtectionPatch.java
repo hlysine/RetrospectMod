ef6ce7bd51c16e4cb58b5a948914009586849dc5
Henry Lin <henry.ys.lin@gmail.com>
2022-06-13 22:52:10 +0800

    Add IntoTheVoid

---
+++ src/main/java/theRetrospect/patches/OnCreatureDeathPreProtectionPatch.java
@@ -0,0 +1,88 @@
+package theRetrospect.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import theRetrospect.subscribers.OnCreatureDeathPreProtectionSubscriber;
+
+public class OnCreatureDeathPreProtectionPatch {
+
+    @SpirePatch(
+            clz = AbstractPlayer.class,
+            method = "damage"
+    )
+    public static class OnPlayerDeathPreProtectionPatch {
+
+        @SpireInsertPatch(
+                locator = Locator.class
+        )
+        public static SpireReturn<Void> Insert(AbstractPlayer __instance, DamageInfo info) {
+            boolean canDie = true;
+            for (AbstractPower power : __instance.powers) {
+                if (power instanceof OnCreatureDeathPreProtectionSubscriber) {
+                    OnCreatureDeathPreProtectionSubscriber subscriber = (OnCreatureDeathPreProtectionSubscriber) power;
+                    canDie = subscriber.onPlayerDeathPreProtection(info) && canDie;
+                }
+            }
+            for (AbstractRelic relic : __instance.relics) {
+                if (relic instanceof OnCreatureDeathPreProtectionSubscriber) {
+                    OnCreatureDeathPreProtectionSubscriber subscriber = (OnCreatureDeathPreProtectionSubscriber) relic;
+                    canDie = subscriber.onPlayerDeathPreProtection(info) && canDie;
+                }
+            }
+            if (canDie) {
+                return SpireReturn.Continue();
+            } else {
+                return SpireReturn.Return();
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "hasRelic");
+
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+
+
+    @SpirePatch(
+            clz = AbstractMonster.class,
+            method = "damage"
+    )
+    public static class OnMonsterDeathPreProtectionPatch {
+
+        @SpireInsertPatch(
+                locator = Locator.class
+        )
+        public static SpireReturn<Void> Insert(AbstractMonster __instance, DamageInfo info) {
+            boolean canDie = true;
+            for (AbstractPower power : __instance.powers) {
+                if (power instanceof OnCreatureDeathPreProtectionSubscriber) {
+                    OnCreatureDeathPreProtectionSubscriber subscriber = (OnCreatureDeathPreProtectionSubscriber) power;
+                    canDie = subscriber.onPlayerDeathPreProtection(info) && canDie;
+                }
+            }
+            if (canDie) {
+                return SpireReturn.Continue();
+            } else {
+                return SpireReturn.Return();
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractMonster.class, "die");
+
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+}
---
+++ src/main/java/theRetrospect/subscribers/OnCreatureDeathPreProtectionSubscriber.java
@@ -0,0 +1,14 @@
+package theRetrospect.subscribers;
+
+import com.megacrit.cardcrawl.cards.DamageInfo;
+
+public interface OnCreatureDeathPreProtectionSubscriber {
+    /**
+     * Triggered when the creature is about to die.
+     * This is triggered before protections such as Lizard Tail and Fairy In A Bottle take effect.
+     *
+     * @param info The {@link DamageInfo} that caused the death.
+     * @return Whether to allow the creature to die.
+     */
+    boolean onPlayerDeathPreProtection(DamageInfo info);
+}
