4287183c2b3c88aba846948dbcb73ab77ea9819e
Henry Lin <henry.ys.lin@gmail.com>
2022-06-15 10:30:21 +0800

    Add EnergizedPower icon

--- src/main/java/theRetrospect/powers/EnergizedPower.java
+++ src/main/java/theRetrospect/powers/EnergizedPower.java
@@ -1,6 +1,8 @@
 package theRetrospect.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -8,6 +10,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import theRetrospect.RetrospectMod;
+import theRetrospect.util.TextureLoader;
 
 public class EnergizedPower extends AbstractPower implements CloneablePowerInterface {
 
@@ -15,6 +18,9 @@ public class EnergizedPower extends AbstractPower implements CloneablePowerInter
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture tex84 = TextureLoader.getTexture("theRetrospectResources/images/powers/energized84.png");
+    private static final Texture tex32 = TextureLoader.getTexture("theRetrospectResources/images/powers/energized32.png");
 
     public EnergizedPower(final AbstractCreature owner, final int amount) {
         name = NAME;
@@ -26,7 +32,8 @@ public class EnergizedPower extends AbstractPower implements CloneablePowerInter
         type = PowerType.BUFF;
         isTurnBased = false;
 
-        loadRegion("energized_blue"); // TODO: use custom energized energy icon
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
         updateDescription();
     }
