08dc200cc4f9da7f597b75abf346d694f7c994b1
Henry Lin <henry.ys.lin@gmail.com>
2022-06-24 22:42:08 +0800

    Add AssetProcurement

---
+++ src/main/java/theRetrospect/cards/skills/AssetProcurement.java
@@ -0,0 +1,24 @@
+package theRetrospect.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRetrospect.RetrospectMod;
+import theRetrospect.cards.AbstractBaseCard;
+
+public class AssetProcurement extends AbstractBaseCard {
+
+    public static final String ID = RetrospectMod.makeID(AssetProcurement.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.NONE;
+
+    public AssetProcurement() {
+        super(ID, TARGET);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new MakeTempCardInHandAction(AbstractDungeon.returnTrulyRandomColorlessCardInCombat()));
+    }
+}
