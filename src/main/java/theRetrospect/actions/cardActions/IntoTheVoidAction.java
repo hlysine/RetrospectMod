66fdf5c5a4cafef3325c36818c31b8896f5ea5d4
Henry Lin <henry.ys.lin@gmail.com>
2022-06-26 13:09:34 +0800

    Add IntoTheVoid

---
+++ src/main/java/theRetrospect/actions/cardActions/IntoTheVoidAction.java
@@ -0,0 +1,25 @@
+package theRetrospect.actions.cardActions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theRetrospect.powers.IntoTheVoidPower;
+
+public class IntoTheVoidAction extends AbstractGameAction {
+    private final int hpLoss;
+
+    public IntoTheVoidAction(int hpLoss) {
+        this.hpLoss = hpLoss;
+    }
+
+    @Override
+    public void update() {
+        AbstractPlayer player = AbstractDungeon.player;
+        addToTop(new LoseHPAction(player, player, hpLoss));
+        if (!AbstractDungeon.player.hasPower(IntoTheVoidPower.POWER_ID))
+            addToTop(new ApplyPowerAction(player, player, new IntoTheVoidPower(player, 1)));
+        this.isDone = true;
+    }
+}
