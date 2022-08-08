7d6aecf26dd500d1d7626270c780c4b4908e1d01
Henry Lin <henry.ys.lin@gmail.com>
2022-08-08 21:17:55 +0800

    Add Evanescence power icon

--- src/main/java/theRetrospect/powers/EvanescencePower.java
+++ src/main/java/theRetrospect/powers/EvanescencePower.java
@@ -20,8 +20,8 @@ public class EvanescencePower extends AbstractPower implements CloneablePowerInt
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    private static final Texture tex84 = TextureLoader.getTexture("theRetrospectResources/images/powers/placeholder_power84.png");
-    private static final Texture tex32 = TextureLoader.getTexture("theRetrospectResources/images/powers/placeholder_power32.png");
+    private static final Texture tex84 = TextureLoader.getTexture("theRetrospectResources/images/powers/evanescence84.png");
+    private static final Texture tex32 = TextureLoader.getTexture("theRetrospectResources/images/powers/evanescence32.png");
 
     public EvanescencePower(final AbstractCreature owner, final int amount) {
         name = NAME;
