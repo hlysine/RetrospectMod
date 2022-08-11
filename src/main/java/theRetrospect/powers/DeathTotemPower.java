d67a780101632899205ebcddd78e9293405b81a8
Henry Lin <henry.ys.lin@gmail.com>
2022-08-11 11:37:02 +0800

    Add Death Totem power icon

--- src/main/java/theRetrospect/powers/DeathTotemPower.java
+++ src/main/java/theRetrospect/powers/DeathTotemPower.java
@@ -21,8 +21,8 @@ public class DeathTotemPower extends AbstractPower implements CloneablePowerInte
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    private static final Texture tex84 = TextureLoader.getTexture("theRetrospectResources/images/powers/placeholder_power84.png");
-    private static final Texture tex32 = TextureLoader.getTexture("theRetrospectResources/images/powers/placeholder_power32.png");
+    private static final Texture tex84 = TextureLoader.getTexture("theRetrospectResources/images/powers/death_totem84.png");
+    private static final Texture tex32 = TextureLoader.getTexture("theRetrospectResources/images/powers/death_totem32.png");
 
     public DeathTotemPower(final AbstractCreature owner, final int amount) {
         name = NAME;
