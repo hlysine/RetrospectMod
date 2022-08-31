637f4089d91602f881a922285d2c5c1478a7e372
Henry Lin <henry.ys.lin@gmail.com>
2022-08-31 18:52:29 +0800

    Fix card follow-up actions not executed when there are no valid targets on screen

--- src/main/java/theRetrospect/patches/CardFollowUpActionsPatch.java
+++ src/main/java/theRetrospect/patches/CardFollowUpActionsPatch.java
@@ -1,24 +1,67 @@
 package theRetrospect.patches;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
 import theRetrospect.util.CardUtils;
 
 /**
- * Trigger actionAfterUse of a card and clean card fields.
+ * Trigger follow-up actions of a card and clean card fields.
  */
-@SpirePatch(
-        clz = UseCardAction.class,
-        method = "update"
-)
 public class CardFollowUpActionsPatch {
-    public static void Postfix(UseCardAction __instance, AbstractCard ___targetCard) {
-        if (__instance.isDone) {
-            CardUtils.setPlaySource(___targetCard, null);
-            CardUtils.setReturnToMinion(___targetCard, null);
+    private static void triggerFollowUpActions(AbstractCard card) {
+        CardUtils.setPlaySource(card, null);
+        CardUtils.setReturnToMinion(card, null);
 
-            CardUtils.createFollowUpActionHandler(___targetCard).scheduleFollowUpActions();
+        CardUtils.createFollowUpActionHandler(card).scheduleFollowUpActions();
+    }
+
+    /**
+     * Trigger follow-up actions after a card is successfully played.
+     */
+    @SpirePatch(
+            clz = UseCardAction.class,
+            method = "update"
+    )
+    public static class UseCardActionPatch {
+        public static void Postfix(UseCardAction __instance, AbstractCard ___targetCard) {
+            if (__instance.isDone) {
+                triggerFollowUpActions(___targetCard);
+            }
+        }
+    }
+
+    /**
+     * Trigger follow-up actions after a card is auto-played but has no targets to act on.
+     * For some reason UseCardAction is not enqueued for this specific case.
+     */
+    @SpirePatch(
+            clz = GameActionManager.class,
+            method = "getNextAction"
+    )
+    public static class ActionManagerGetNextActionPatch {
+        @SpireInsertPatch(
+                locator = Locator.class
+        )
+        public static void Insert(GameActionManager __instance) {
+            CardQueueItem item = __instance.cardQueue.get(0);
+            if (item.card != null && item.autoplayCard) {
+                triggerFollowUpActions(item.card);
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractMonster.class, "isDeadOrEscaped");
+
+                return new int[]{LineFinder.findInOrder(ctMethodToPatch, finalMatcher)[0] + 1};
+            }
         }
     }
 }
