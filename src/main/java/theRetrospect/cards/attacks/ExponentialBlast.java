7a115cefad10dc202f99247a66be84353e2259d4
Henry Lin <henry.ys.lin@gmail.com>
2022-09-01 10:30:49 +0800

    Rework Exponential Blast to be an X cost card

--- src/main/java/theRetrospect/cards/attacks/ExponentialBlast.java
+++ src/main/java/theRetrospect/cards/attacks/ExponentialBlast.java
@@ -5,12 +5,12 @@ import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import com.megacrit.cardcrawl.vfx.combat.MindblastEffect;
 import theRetrospect.RetrospectMod;
 import theRetrospect.cards.AbstractBaseCard;
+import theRetrospect.util.CardUtils;
 
 public class ExponentialBlast extends AbstractBaseCard {
 
@@ -24,23 +24,32 @@ public class ExponentialBlast extends AbstractBaseCard {
         this.exhaust = true;
     }
 
-    private int calculateDamage() {
-        int count = (int) Math.pow(2, EnergyPanel.getCurrentEnergy());
+    private int calculateDamage(int x) {
+        int count = (int) Math.pow(2, x);
         this.damage = this.baseDamage = count;
         return count;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.player.loseEnergy(this.magicNumber);
+        int effect = CardUtils.calculateXCostEffect(this);
 
-        calculateDamage();
-        this.applyPowers();
+        effect -= this.magicNumber;
 
-        if (m != null) {
-            addToBot(new VFXAction(new MindblastEffect(p.dialogX, p.dialogY, p.flipHorizontal)));
+        if (effect > 0) {
+            if (!this.freeToPlayOnce) {
+                p.energy.use(EnergyPanel.getCurrentEnergy());
+            }
+
+            calculateDamage(effect);
+            this.applyPowers();
+
+
+            if (m != null) {
+                addToBot(new VFXAction(new MindblastEffect(p.dialogX, p.dialogY, p.flipHorizontal)));
+            }
+
+            addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
         }
-
-        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
     }
 }
