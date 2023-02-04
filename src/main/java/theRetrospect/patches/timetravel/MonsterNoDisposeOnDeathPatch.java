30b0ba1592913ed188775787bd20127ae8ee7c7f
Henry Lin <henry.ys.lin@gmail.com>
2023-02-04 11:31:49 +0800

    Fix class name in instrument patch

--- src/main/java/theRetrospect/patches/timetravel/MonsterNoDisposeOnDeathPatch.java
+++ src/main/java/theRetrospect/patches/timetravel/MonsterNoDisposeOnDeathPatch.java
@@ -5,15 +5,17 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 
-@SpirePatch(clz = AbstractMonster.class, method = "updateDeathAnimation")
-
+@SpirePatch(
+        clz = AbstractMonster.class,
+        method = "updateDeathAnimation"
+)
 public class MonsterNoDisposeOnDeathPatch {
 
     public static ExprEditor Instrument() {
         return new ExprEditor() {
             public void edit(javassist.expr.MethodCall m) throws CannotCompileException {
                 if (m.getMethodName().equals("dispose")) {
-                    m.replace("{ theRetrospect.mechanics.timetravel.StateManager.scheduleDisposable(this); }");
+                    m.replace("{ theRetrospect.mechanics.timetravel.TimeManager.scheduleDisposable(this); }");
                 }
             }
         };
