1c9fbd5c4bf2806b5f7650d91a4acc08a6c88f15
Henry Lin <henry.ys.lin@gmail.com>
2023-02-06 10:26:34 +0800

    Add test card for unstable RNG

---
+++ src/main/java/theRetrospect/cards/attacks/UnstableTest.java
@@ -0,0 +1,32 @@
+package theRetrospect.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theRetrospect.RetrospectMod;
+import theRetrospect.cards.AbstractBaseCard;
+import theRetrospect.mechanics.timetravel.TimeManager;
+
+public class UnstableTest extends AbstractBaseCard {
+
+    public static final String ID = RetrospectMod.makeID(UnstableTest.class.getSimpleName());
+
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+
+    public UnstableTest() {
+        super(ID, TARGET);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int roll = TimeManager.unstableRng.random(magicNumber - 1);
+        if (roll == 0) {
+            addToBot(new HealAction(m, p, damage));
+        } else {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        }
+    }
+}
